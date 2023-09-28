94bc28080c0da50065b13313eb30d5f8c71910b9
TeoCalvo <TeoCalvo>
2023-09-28 22:32:36 +0000

    Desafio

--- dia02/Bora Praticar.sql
+++ dia02/Bora Praticar.sql
@@ -35,3 +35,37 @@ SELECT *,
 FROM silver.olist.pedido
 
 WHERE datediff(dtEstimativaEntrega, dtEntregue) >= 2
+
+-- COMMAND ----------
+
+-- Selecione a tabela silver.olist.produto :
+-- Crie uma coluna nova chamada ‘descNovaCategoria’ seguindo:
+-- agrupe ‘alimentos’ e ‘alimentos_bebidas’ em ‘alimentos’
+-- agrupe ‘artes’ e ‘artes_e_artesanato’ em ‘artes’
+-- agrupe todas categorias de construção em uma única categoria chamada ‘construcao’
+-- Cria uma coluna nova chamada ‘descPeso’
+-- para peso menor que 2kg: ‘leve’
+-- para peso entre 2kg e 5kg: ‘médio’
+-- para peso entre 5kg e 10kg: ‘pesado’
+-- para peso maior que 10kg: ‘muito pesado’
+
+SELECT *,
+       CASE
+          WHEN descCategoria IN ('alimentos', 'alimentos_bebidas') THEN 'alimentos'
+          WHEN descCategoria IN ('artes', 'artes_e_artesanato') THEN 'artes'
+          WHEN descCategoria LIKE '%construcao%' THEN 'construcao'
+          ELSE descCategoria
+       END AS descNovaCategoria,
+
+       CASE
+          WHEN vlPesoGramas < 2000 THEN 'leve'
+          WHEN vlPesoGramas < 5000 THEN 'medio'
+          WHEN vlPesoGramas < 10000 THEN 'pesado'
+          ELSE 'muito pesado'
+       END AS descPeso
+
+FROM silver.olist.produto
+
+-- COMMAND ----------
+
+
